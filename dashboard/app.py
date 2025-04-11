b45cca1e7c74cabb33bde14479ad59166d3cb387
ja'far shodiq <potanut12@gmail.com>
2025-04-11 23:22:20 +0800

    update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import frontend,test 
+from apps import frontend, test 
 
 # Set page configuration
 st.set_page_config(page_title='Sentiment Analysis Dashboard', layout='wide')
@@ -9,7 +9,7 @@ app = MultiApp()
 
 st.markdown("""
 # Sentiment Analysis Dashboard
-Dashboard ini berisi analisis sentimen komentar netizen di Twitter terhadap gaji dan kesehatan mental generasi Z.
+Dashboard ini berisi analisis sentimen komentar netizen di Twitter terhadap gaji mempengaruhi kesehatan mental generasi Z.
 """)
 
 app.add_app("Home", frontend.app)
